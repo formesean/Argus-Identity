6184687105a624110313bbd437cacb0bfa6f658f
formesean <seanaguilar698@gmail.com>
2025-12-30 15:11:42 +0800

    feat: configure Argus Indexer and add reducers for IdentityDbContext

--- backend/src/ArgusIdentity.Indexer/Program.cs
+++ backend/src/ArgusIdentity.Indexer/Program.cs
@@ -1,7 +1,17 @@
+using Argus.Sync.Extensions;
+using Argus.Sync.Data.Models;
+using ArgusIdentity.Indexer.Data;
+using ArgusIdentity.Indexer.Models;
+using ArgusIdentity.Indexer.Reducers;
 using ArgusIdentity.Indexer;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+// Argus Indexer
+builder.Services.AddCardanoIndexer<IdentityDbContext>(builder.Configuration);
+
+// Reducers
+builder.Services.AddReducers<IdentityDbContext, IReducerModel>(builder.Configuration);
 
 var host = builder.Build();
 host.Run();
